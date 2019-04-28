70df482e6df64b115c3ccce61d4aee52fa9f1fe3
Monique_Willis <moniquew08@gmail.com>
2019-04-28 10:36:56 -0400

    javascript1

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+var name1 = "Carol";
+var name2 = "Carlos";
+var name3 = "Johnny";
+
+let choice = prompt("What is your name?");
+
+if (name1.length === name2.length && name3.length === name2.length) {
+    console.log(`All three names ${name1} ${name2} ${name3} have the same length.`);
+} 
+else if (name1.length > name2.length && name1.length > name3.length) {
+    console.log(`${name1} has the longest name.`);
+}
+else if (name2.length > name1.length && name2.length > name3.length) {
+    console.log(`${name2} has the longest name.`);
+}
+else if (name3.length > name2.length && name3.length > name1.length) {
+    console.log(`${name3} has the longest name.`);
+}
+else if (name1.length === name2.length) {
+    console.log(`${name1} and ${name2} tie for the longest name.`);
+}
+else if (name2.length === name3.length) {
+    console.log(`${name2} and ${name3} tie for the longest name.`);
+}
+else if (name3.length === name1.length) {
+    console.log(`${name3} and ${name1} tie for the longest name.`);
+}
